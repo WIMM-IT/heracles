d8e0dd29cf3ccd90010c1270fc5404516eb0ee67
dpag0307 <duncan.tooke@imm.ox.ac.uk>
2024-10-21 09:18:49 +0100

    Adds the ability to use the Hydra API

--- Heracles.Lib/Hydra.cs
+++ Heracles.Lib/Hydra.cs
@@ -1,16 +1,68 @@
-﻿using System.Diagnostics;
+﻿using Csutils;
+using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
-using Csutils;
 
 namespace Heracles.Lib
 {
-	public static class Hydra
-	{
-		public static List<Record>? Read(int limit = 500000)
-		{
-			Process p = new();
-			(string output, string error) = p.Run("hydra.sh", limit.ToString());
-			return JsonSerializer.Deserialize<List<Record>>(output);
-		}
-	}
+    public class HydraClient
+    {
+
+        private HttpClient httpClient;
+        private string? apiToken;
+
+        public HydraClient(string uri)
+        {
+            apiToken = Environment.GetEnvironmentVariable("HYDRA_TOKEN");
+            ArgumentNullException.ThrowIfNull(apiToken);
+
+            httpClient = new();
+            httpClient.BaseAddress = new Uri(uri);
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Basic {apiToken}");
+        }
+
+        private void PrintRecords(List<Record> records)
+        {
+            foreach (var obj in records)
+            {
+                Console.WriteLine(obj.ToPrettyString());
+            }
+        }
+        public async Task<List<Record>?> Get(int limit = 500000, string q = "")
+        {
+            Console.WriteLine($"GET records?limit={limit}&q={q}");
+            List<Record>? records = await httpClient.GetFromJsonAsync<List<Record>>($"records?limit={limit}&q={q}");
+            ArgumentNullException.ThrowIfNull(records);
+            PrintRecords(records);
+            return records;
+        }
+
+        public async Task Delete(string q)
+        {
+            Console.WriteLine($"DELETE records?q=in_hostname%3A{q}&limit=1");
+            var response = await httpClient.DeleteAsync($"records?q=in_hostname%3A{q}&limit=1");
+            List<Record>? records = JsonSerializer.Deserialize<List<Record>>(await response.Content.ReadAsStringAsync());
+            ArgumentNullException.ThrowIfNull(records);
+            PrintRecords(records);
+        }
+
+        public async Task Post(string host, string token)
+        {
+            using StringContent jsonContent = new(
+                JsonSerializer.Serialize(new
+                {
+                    hostname = host,
+                    type = "TXT",
+                    content = token,
+                    comment = "WinAcme"
+                }),
+                Encoding.UTF8,
+                "application/json");
+
+            Console.WriteLine($"POST {host} {token}");
+            var response = await httpClient.PostAsync("records", jsonContent);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
+        }
+    }
 }
