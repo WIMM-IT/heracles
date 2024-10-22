c406a1bd2d04ae0df19da570ee92b431c2f87abb
dpag0307 <duncan.tooke@imm.ox.ac.uk>
2024-10-22 11:55:02 +0100

    Disambiguates the arguments and updates docstrings

--- Heracles.Lib/Hydra.cs
+++ Heracles.Lib/Hydra.cs
@@ -30,18 +30,20 @@ namespace Heracles.Lib
         }
 
         /// <summary>
-        /// Queries Hydra for records matching a search pattern.
-        /// Throws an exception if an unexpected response is recieved.
+        /// Queries Hydra for records where the hostname contains a given substring.
+        /// By default, returns unlimited matching records (up to the 500000 limit imposed by the API).
+        /// Throws an exception if an unexpected response is recieved from the API or the deserialized JSON is null.
         /// </summary>
-        /// <param name="query"></param>
+        /// <param name="substring"></param>
         /// <param name="limit"></param>
         /// <returns>A list of matching records.</returns>
         /// <exception cref="HttpRequestException"></exception>
-        public async Task<List<Record>> Get(string query = "", int limit = 500000)
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<List<Record>> Get(string substring = "", int limit = 500000)
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"-> GET records?q={query}&limit={limit}");
-            var response = await httpClient.GetAsync($"records?q={query}&limit={limit}");
+            Console.WriteLine($"-> GET records?q=in_hostname%3A{substring}&limit={limit}");
+            var response = await httpClient.GetAsync($"records?q=in_hostname%3A{substring}&limit={limit}");
             Console.WriteLine($"<- {response.StatusCode}");
             Console.ResetColor();
 
@@ -59,19 +61,20 @@ namespace Heracles.Lib
         }
 
         /// <summary>
-        /// Deletes records matching a search pattern from Hydra.
-        /// Throws an exception if an unexpected response is recieved.
+        /// Deletes records from Hydra where the hostname contains a given substring.
         /// By default, will only delete the first matching record.
+        /// Throws an exception if an unexpected response is recieved from the API or the deserialized JSON is null.
         /// </summary>
-        /// <param name="query"></param>
+        /// <param name="substring"></param>
         /// <param name="limit"></param>
         /// <returns>A list of deleted records.</returns>
         /// <exception cref="HttpRequestException"></exception>
-        public async Task<List<Record>> Delete(string query, int limit = 1)
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<List<Record>> Delete(string substring, int limit = 1)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"-> DELETE records?q=in_hostname%3A{query}&limit={limit}");
-            var response = await httpClient.DeleteAsync($"records?q=in_hostname%3A{query}&limit={limit}");
+            Console.WriteLine($"-> DELETE records?q=in_hostname%3A{substring}&limit={limit}");
+            var response = await httpClient.DeleteAsync($"records?q=in_hostname%3A{substring}&limit={limit}");
             Console.WriteLine($"<- {response.StatusCode}");
             Console.ResetColor();
 
@@ -90,11 +93,12 @@ namespace Heracles.Lib
 
         /// <summary>
         /// Adds a new record to Hydra.
-        /// Throws an exception if an unexpected response is recieved.
+        /// Throws an exception if an unexpected response is recieved from the API or the deserialized JSON is null.
         /// </summary>
         /// <param name="newRecord"></param>
         /// <returns>A copy of the newly added record.</returns>
         /// <exception cref="HttpRequestException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
         public async Task<Record> Post(Record newRecord)
         {
             ArgumentNullException.ThrowIfNull(newRecord);
