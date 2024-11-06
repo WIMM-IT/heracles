bd72ebe0c4e88107b9cc800ac860e6a87d9fa102
dpag0307 <duncan.tooke@imm.ox.ac.uk>
2024-11-06 10:16:19 +0000

    Adds docstrings

--- Heracles.Lib/RecordHelpers.cs
+++ Heracles.Lib/RecordHelpers.cs
@@ -5,15 +5,36 @@ namespace Heracles.Lib
     public static class RecordHelpers
     {
 
+        /// <summary>
+        /// Serialise a record to its JSON representation.
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns>A JSON string.</returns>
         public static string RecordToJson(Record r) =>
             JsonSerializer.Serialize(r, typeof(Record), SourceGenerationContext.Default);
 
+        /// <summary>
+        /// Serialise a list of records to their JSON representation.
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns>A JSON string.</returns>
         public static string RecordListToJson(List<Record> r) =>
             JsonSerializer.Serialize(r, typeof(List<Record>), SourceGenerationContext.Default);
 
+        /// <summary>
+        /// Deerialise a record from its JSON representation.
+        /// </summary>
+        /// <param name="j"></param>
+        /// <returns>A Hydra Record</returns>
         public static Record? JsonToRecord(string j) =>
             JsonSerializer.Deserialize(j, typeof(Record), SourceGenerationContext.Default)
         as Record;
+
+        /// <summary>
+        /// Deerialise a list of records from their JSON representation.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns>A list of Hydar Records</returns>
         public static List<Record>? JsonToRecordList(string s) =>
             JsonSerializer.Deserialize(s, typeof(List<Record>), SourceGenerationContext.Default)
         as List<Record>;
